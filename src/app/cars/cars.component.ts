c5a60118205ee82872044ceb483f1feaef3ae8d1
Roman-Liubinets <roman.liubinets@gmail.com>
2018-06-26 01:51:33 +0300

    Templates and styles in components

---
+++ src/app/cars/cars.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cars',
+  templateUrl: './cars.component.html',
+  styles: []
+})
+export class CarsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
